7e1c1268bd71737389181fbb1c53750b3191112b
muyannian <myn@lucene.xin>
2021-01-24 17:16:08 +0800

    测试完毕后提交

--- LXTest.java
+++ LXTest.java
@@ -24,6 +24,7 @@ public class BatchRead {
 
 		System.out.println(raw.getClass().getName());
 
+		boolean isskip=args.length>1;
 		if(raw instanceof DFSInputStream)
 		{
 			//这里支持批量读
@@ -59,34 +60,83 @@ public class BatchRead {
 
 				//真正的使用API
 				batch.readFully(position, buffer, start, length);
+				
+				
 				long ts1=System.currentTimeMillis();
 
-				boolean cmp_result=true;
-				int cnt=0;
-//				//结果比对
-				
 
-				for(int j=0;j<batchCnt;j++)
+				if(!isskip)
 				{
-					byte[] buff=new byte[length[j]];
-					buffers[j]=buff;
-					input.readFully(position[j], buff,0,length[j]);
-				
-				}
-//				
-				long ts2=System.currentTimeMillis();
-//				
-				for(int j=0;j<batchCnt;j++)
-				{
-					if(!Arrays.equals(buffers[j], buffer[j]))
+
+					for(int j=0;j<batchCnt;j++)
 					{
-						cmp_result=false;
-					}else {
-						cnt++;
+						byte[] buff=new byte[length[j]];
+						buffers[j]=buff;
+						input.readFully(position[j], buff,0,length[j]);
+						
+						
+					
 					}
-				}
+					
+					boolean cmp_result=true;
+					int cnt=0;
+//					//结果比对
+					for(int j=0;j<batchCnt;j++)
+					{
+						if(!Arrays.equals(buffers[j], buffer[j]))
+						{
+							cmp_result=false;
+						}else {
+							cnt++;
+						}
+					}
+
+					pos=(long) (Math.random()*max_position);
+					for(int j=0;j<batchCnt;j++)
+					{
+						position[j]=1+pos+(long) (Math.random()*1024*1024*128);//随机数不能越界 //1+(long) (Math.random()*max_position);//
+						
+						int len=64;//(int) (Math.random()*1024);
+						if(buffer[j]==null)
+						{
+							buffer[j]=new byte[len];
+						}
+						start[j]=0;
+						length[j]=len;
+					}
+
+			
+					
+
+					
+					//换个位置,但同样大小的数据对比,目的是缓存因素
+					long ts3=System.currentTimeMillis();
+
+					
+					for(int j=0;j<batchCnt;j++)
+					{
+						byte[] buff=new byte[length[j]];
+						buffers[j]=buff;
+						input.readFully(position[j], buff,0,length[j]);
+					
+					}
+					
+					long ts4=System.currentTimeMillis();
+
+
 				
-				System.out.println("cmpdiff:比对结果:"+cmp_result+"@校验成功次数:"+cnt+"@读取次数:"+batchCnt+"@优化后时间:"+(ts1-ts)+"@原始hdfs时间:"+(ts2-ts1));
+					System.out.println("cmpdiff:比对结果:"+cmp_result+"@校验成功次数:"+cnt+"@读取次数:"+batchCnt+"@优化后时间:"+(ts1-ts)+"@原始hdfs时间:"+(ts4-ts3));
+
+					
+				}else {
+					System.out.println("cmpdiff:读取次数:"+batchCnt+"@优化后时间:"+(ts1-ts));
+
+				}
+
+		
+				
+		
+				
 
 				
 			}
